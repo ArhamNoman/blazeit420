8180e83201dcd7d245fcf26a343cab326ce31fbb
Syed Mohammad Arham Noman <20100059@lums.edu.pk>
2019-05-16 04:24:57 +0500

    Added tests for login component

--- client/src/__tests__/App.test.js
+++ client/src/__tests__/App.test.js
@@ -3,10 +3,19 @@ import {shallow} from 'enzyme';
 import React from 'react'; 
 import toJson from 'enzyme-to-json'; 
 
-describe('App', () => {
+const component = shallow(<App />);
+
+describe('Login Component', () => {
     it('renders without crashing given the required props', () => {
         const props = {}
         const wrapper = shallow(<App {...props} />)
         expect(toJson(wrapper)).toMatchSnapshot()
+    })
+    
+    it('Login provides two login options', () => {
+        const wrapper = component.find('ProtectedRoute');
+        expect(wrapper.length).toBe(2);
+        // console.log(component.debug())
+
     })
 })
