52325390d5af065bcf77bca5509240fdc56c01a9
Syed Mohammad Arham Noman <20100059@lums.edu.pk>
2019-05-15 12:04:44 +0500

    Added a test for app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Redirect, Switch, Route} from 'react-router-dom'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Login from './Components/Login'
 import AdminDashboard from './Components/Admin/AdminPanel'
 import FacultyDashboard from './Components/Faculty/FacultyPanel'
@@ -25,16 +25,18 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Switch>
-          <Route exact path="/" component={Login}/>
-          <ProtectedRoute path="/admin" component={AdminDashboard}/>
-          <ProtectedRoute path="/faculty" component={FacultyDashboard}/>
-          <Route path = "*" component={NotFound} />
-        </Switch>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Login}/>
+            <ProtectedRoute path="/admin" component={AdminDashboard}/>
+            <ProtectedRoute path="/faculty" component={FacultyDashboard}/>
+            <Route path = "*" component={NotFound} />
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
 }
 
 export default App
-export {App} ; 
+export {App} 
